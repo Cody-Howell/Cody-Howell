d715465f0650c3b6e8dc10d5f847997f095364ee
Cody Howell <codyhowell44@gmail.com>
2022-06-18 18:08:31 -0600

    Not sure if first or second commit.

--- Programming Things/Game for Programming Fundamentals/Program.cs
+++ Programming Things/Game for Programming Fundamentals/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Date: Unknown
+Name: Cody Howell
+Lab: Final Project *hopefully*/
+Console.Clear();
+
+Console.WriteLine(@"Hello, and welcome to the game. You are a [sprite not found], wandering around a map.
+You'll have to defeat random enemies, solve a maze, and beat the boss at the end. Good luck!
+Press any key to begin.");
+Console.ReadKey(true);
+
+
