57483335a7749dffcf898d8349af0d88a1a67fc0
Cody Howell <codyhowell44@gmail.com>
2022-06-23 10:06:04 -0600

    Final commit until further notice, did some game adjusting, think everything is working correctly.

--- Programming Things/Game for Programming Fundamentals/Program.cs
+++ Programming Things/Game for Programming Fundamentals/Program.cs
@@ -11,6 +11,12 @@ static void TryMove(int proposedTop, int proposedLeft, string[] screen){ //reusi
     if (proposedLeft < screen[0].Length && proposedLeft > 0 && screen[proposedTop][proposedLeft] != '^' && screen[proposedTop][proposedLeft] != '#')
         {Console.CursorLeft = proposedLeft;}
 }
+static void DrawMap(string[] screen){ //So I can have a second method.
+    Console.Clear();
+    foreach (string line in screen){
+        Console.WriteLine(line);
+    }
+}
 
 //Initialization
 Random rand = new Random();
@@ -22,7 +28,6 @@ bool inBattle = false;
 bool fromBattle = false;
 bool winning = false;
 int[] standing = new int[2];
-bool dead = true;
 int health = 100;
 int BATTLE = 0;
 Stopwatch stopwatch = new Stopwatch();
@@ -37,14 +42,11 @@ Console.ReadKey(true);
 stopwatch.Start();
 
 do {
-    string[] screen = new string[20];
+    string[] screen = new string[20]; //This was placed inside the loop to continually draw the maps used.
     string[] mapPath = new string[7] {"Map.txt" , "Maze 1.txt" , "Maze 2.txt" , "Maze 3.txt" , "Maze 4.txt" , "Boss.txt" , "Random Encounters.txt"};
     screen = File.ReadAllLines(mapPath[map]);
-    Console.Clear();
-    foreach (string line in screen){
-        Console.WriteLine(line);
-    }
-    
+    DrawMap(screen);
+    //This section is the complicated bit about where you came from, and where you should go.
     if (fromBattle == true){Console.CursorTop = standing[0]; Console.CursorLeft = standing[1]; fromBattle = false;}
     else if (checkPointReached == false){Console.CursorTop = 3; Console.CursorLeft = 4;}
     else if (map == 5){Console.CursorTop = 16; Console.CursorLeft = 24;}
@@ -53,7 +55,7 @@ do {
 
     while (true){
         var dir = Console.ReadKey(true).Key; 
-        switch (dir){ //Second stolen thing, going to clean it with a Switch function, hopefully.
+        switch (dir){ //Second stolen thing, going to clean it with a Switch function.
             case ConsoleKey.UpArrow:
                 TryMove(Console.CursorTop-1, Console.CursorLeft, screen);
                 break;
@@ -68,11 +70,13 @@ do {
                 break;
             //You don't get an escape key. :/
         }
-        if (screen[Console.CursorTop][Console.CursorLeft] == '!'){ 
+
+        if (screen[Console.CursorTop][Console.CursorLeft] == '!'){ //Theoretically these could all be else if's, but I'm scared that might break something.
             Console.Clear();
             Console.WriteLine("You died! Don't touch those => !");
             Console.WriteLine("Press a key to respawn.");
             Console.ReadKey(true);
+            health = 100;
             map = 0;
             inBoss = false;
             break;
@@ -97,6 +101,7 @@ do {
             map = rand.Next(1,4); 
             break;
         }
+        //Check to see if a random encounter should happen.
         BATTLE = rand.Next(1,30);
         if (BATTLE == 6 && map == 0){
             break;
@@ -104,37 +109,24 @@ do {
     }
     //Start of the random encounter stuff.
     if (BATTLE == 6 && map == 0){
-        if (health <= 0){
-            Console.Clear();
-            Console.WriteLine("You Died from Random Encounter. Press a key to continue."); 
-            Console.ReadKey(true);
-            map = 0; 
-            checkPointReached = false; 
-            health = 100; 
-            continue;
-        }
         standing[0] = Console.CursorTop;
-        standing[1] = Console.CursorLeft;
+        standing[1] = Console.CursorLeft; //Saving a checkpoint for respawning at the same spot.
         screen = File.ReadAllLines(mapPath[6]);
         Console.Clear();
-        foreach (string line in screen){
-            Console.WriteLine(line);
-        }
+        DrawMap(screen);
         inBattle = true;
-        int enemyHealth = rand.Next(20,50);
+        int enemyHealth = rand.Next(20,40);
 
         do {
             Console.CursorTop = 12; Console.CursorLeft = 35;
             Console.WriteLine(health + " ");
             Console.CursorTop = 4; Console.CursorLeft = 20;
-            Console.WriteLine(enemyHealth + "  ");
+            Console.WriteLine(enemyHealth + "  "); //Placing health inside the graphics.
             
-            dead = false;
-            int attack = rand.Next(5,15);
-            Console.CursorTop = 17;
-            Console.CursorLeft = 0;
+            int attack = rand.Next(5,15); //Variable every time.
+            Console.CursorTop = 17; Console.CursorLeft = 0;
             Console.WriteLine("What would you like to do? Attack, defend, or heal?");
-            string answer = Console.ReadLine().ToLower(); 
+            string answer = ""; 
             bool correct = false; //this is just something dumb to check if they put in an apropriate answer.
             while (correct == false){
                 switch (answer) {
@@ -148,6 +140,10 @@ do {
                     correct = true;
                     break;
                 default:
+                    Console.CursorTop = 18; Console.CursorLeft = 0;
+                    Console.WriteLine("                         ");
+                    Console.CursorTop = 18; Console.CursorLeft = 0;
+                    answer = Console.ReadLine().ToLower(); 
                     break;
                 }
             }
@@ -167,38 +163,48 @@ do {
                 attack /= 2;
                 enemyAttack = 0;
             }
+
             if (answer == "defend"){
                 enemyAttack /= (2 * eC);
                 attack = 0;
             }
             else if (answer == "heal"){
-                health += rand.Next(20,30); //Needed a major buff.
+                health += rand.Next(15,25); //Needed a major buff.
                 attack = 0;
             }
-            Console.WriteLine(health + " " + enemyAttack + " " + enemyHealth + " " + attack + "        ");
+
+            //Console.WriteLine(health + " " + enemyAttack + " " + enemyHealth + " " + attack + "        "); Debug line.
             health -= enemyAttack; //I thought about adding critical hits. No.
             enemyHealth -= attack;
 
             if (enemyHealth <= 0){
                 inBattle = false;
-                dead = false;
                 map = 0;
             }
             if (health <= 0){
                 inBattle = false;
-                dead = true;
                 checkPointReached = false;
                 map = 0;
                 break;
             }
 
-            Console.CursorTop = 18;
-            Console.CursorLeft = 0;
+            Console.CursorTop = 18; Console.CursorLeft = 0;
             Console.WriteLine("                         "); //Erases the writing line.
 
         } while (inBattle == true);
+
         map = 0;
         fromBattle = true;
+        if (health <= 0){
+            Console.Clear();
+            Console.WriteLine("You Died from Random Encounter. Press a key to continue."); 
+            Console.ReadKey(true);
+            fromBattle = false;
+            map = 0; 
+            checkPointReached = false; 
+            health = 100; 
+            continue;
+        }
     }
 } while (winning == false);
 
